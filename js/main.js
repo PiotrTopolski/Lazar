38d1b1daf6c2e8cd2857ef1fb0f2b00f0a3ae21b
Piotr Topolski <piotr@macbook-pro-piotr.home>
2022-03-19 15:13:49 +0100

    add mcjs

---
+++ js/main.js
@@ -0,0 +1,34 @@
+const masonry = new Macy({
+  container: '.column',
+  mobileFirst: true,
+  columns: 2,
+  margin: {
+    x: 4,
+    y: 4
+  },
+  breakAt: {
+    757: {
+      margin: {
+        x: 14,
+        y: 14,
+      },
+      columns: 3
+    },
+    1440: {
+      margin: {
+        x: 14,
+        y: 14,
+      },
+      columns: 4
+    },
+    1620: {
+      margin: {
+        x: 14,
+        y: 14,
+      },
+      columns: 5
+    }
+  }
+})
+
+macy_instance.runOnImageLoad(function () { macy_instance.recalculate(true); }, true);
