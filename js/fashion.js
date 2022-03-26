5895dcc69221d8267103654fc8eaba469a09b9ed
Piotr Topolski <piotr@macbook-pro-piotr.home>
2022-03-26 19:04:47 +0100

    grid update

--- js/fashion.js
+++ js/fashion.js
@@ -34,3 +34,29 @@ backBtn.addEventListener("click", () => {
     item.classList.remove("--active");
   })
 })
+
+const masonryFs = new Macy({
+  container: '.column.--fashion',
+  mobileFirst: true,
+  columns: 2,
+  margin: {
+    x: 4,
+    y: 4
+  },
+  breakAt: {
+    757: {
+      margin: {
+        x: 14,
+        y: 14,
+      },
+      columns: 3
+    },
+    1440: {
+      margin: {
+        x: 14,
+        y: 14,
+      },
+      columns: 4
+    }
+  }
+})
